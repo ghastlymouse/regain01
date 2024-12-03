8085bd6e8d10d36ee454f6bf8b3e0a0ad6015875
Ghastly Mouse <gusdnr0839@gmail.com>
2024-12-03 14:40:38 +0900

    add: firebase 설정

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4x78akXgUdv4UcGJaBzvlpc0uj_AnlHc",
+  authDomain: "nwitter-reloaded-a2489.firebaseapp.com",
+  projectId: "nwitter-reloaded-a2489",
+  storageBucket: "nwitter-reloaded-a2489.firebasestorage.app",
+  messagingSenderId: "401515948784",
+  appId: "1:401515948784:web:4b2726b948a079bcb77035",
+};
+
+const app = initializeApp(firebaseConfig);
