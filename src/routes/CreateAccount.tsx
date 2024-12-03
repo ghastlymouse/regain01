db555292b0f6e03db8eef67860b7d499a802ce34
Ghastly Mouse <gusdnr0839@gmail.com>
2024-12-03 16:42:38 +0900

    update: 로그인 추가

--- src/routes/CreateAccount.tsx
+++ src/routes/CreateAccount.tsx
@@ -1,54 +1,16 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import styled from "styled-components";
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
-
-// const errors = {
-//   "auth/email-already-in-use": "해당 이메일은 이미 존재합니다."
-// }
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/AuthComponents";
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -136,6 +98,9 @@ export default function CreateAccount() {
         />
       </Form>
       {error != "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an accout? <Link to={"/login"}>Log in &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,3 +1,78 @@
+import { useState } from "react";
+import { Form, Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import {
+  Error,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/AuthComponents";
+
 export default function Login() {
-  return <h1>login</h1>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    if (isLoading || email.trim() === "" || password.trim() === "") return;
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+    console.log(name, email, password);
+  };
+  return (
+    <Wrapper>
+      <Title>Log into Nwitter</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="email"
+          value={email}
+          onChange={onChange}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          name="password"
+          value={password}
+          onChange={onChange}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error != "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an accout?{" "}
+        <Link to={"/create-account"}>Create one &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
