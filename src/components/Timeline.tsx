b1f5d54ddba53fd0f29cd890fcc64b53523b86a6
Ghastly Mouse <gusdnr0839@gmail.com>
2024-12-04 16:12:41 +0900

    update: query 작성

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,3 +1,36 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
+
+export type TweetType = {
+  id: string;
+  photo: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+};
+
+const Wrapper = styled.div``;
+
 export default function Timeline() {
-  return <></>;
+  const [tweets, setTweets] = useState<TweetType[]>([]);
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createdAt", "desc")
+    );
+    const snapshot = await getDocs(tweetsQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return { id: doc.id, tweet, createdAt, userId, username, photo };
+    });
+    setTweets(tweets);
+  };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return <Wrapper>{JSON.stringify(tweets)}</Wrapper>;
 }
