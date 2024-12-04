27b810c4b559a3cc7589a78a1b268b67150b0657
Ghastly Mouse <gusdnr0839@gmail.com>
2024-12-04 15:46:47 +0900

    update: db, storage 파일 업로드

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+import PostTweetForm from "../components/PostTweetForm";
+
+const Wrapper = styled.div``;
+
 export default function Home() {
-  return <h1>Home!</h1>;
+  return (
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
+  );
 }
